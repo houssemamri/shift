2ae6724c2a6a9125b5b91a0987798cd3fd1ea2a8
sanket1203 <36723271+sanket1203@users.noreply.github.com>
2019-03-18 11:59:53 +0530

    Update Product_migration_controller.php

--- application/controllers/Product_migration_controller.php
+++ application/controllers/Product_migration_controller.php
@@ -14,6 +14,7 @@ class Product_migration_controller extends CI_Controller {
       parent::__construct();
       $this->load->model('user');
       $this->load->model('Product_migration_model');
+      $this->load->helper(array('db_dinamic_helper'));
     }
 
     public function get_product_migration_view_one(){
@@ -40,76 +41,29 @@ class Product_migration_controller extends CI_Controller {
       $data['opencart_url'] = $this->input->post('opencart_website_url');
 
       // Create Magento database connection.
-      $data['magento_website_full_details'] = $this->Product_migration_model->get_selected_magento_website_details($data['magento_url']);
-      foreach ($data['magento_website_full_details'] as $key => $value) {
-        switch ($key) {
-          case 'magento_database':
-            $config['database'] = "$value";
-            break;
-          case 'magentodbusername':
-            $config['username'] = "$value";
-            break;
-          case 'magento_dbpassword':
-            $config['password'] = "$value";
-            break;
-          case 'magento_dbhost':
-            $config['hostname'] = "$value";
-            break;
-          case 'magento_admin':
-            $data['magento_website_admin'] = $value;
-            break;
-          case 'magento_admin_password':
-            $data['magento_website_admin_password'] = $value;
-            break;
-        }
-      }
-      $config['dbdriver'] = 'mysqli';
-      $config['dbprefix'] = '';
-      $config['pconnect'] = TRUE;
-      $config['db_debug'] = TRUE;
-      $config['cache_on'] = FALSE;
-      $config['cachedir'] = '';
-      $config['char_set'] = 'utf8';
-      $config['dbcollat'] = 'utf8_general_ci';
-      $data['magento_database_connection_status'] = $magento_database = $this->load->database($config, TRUE);
-
-      // Create OpenCart database connection.
-      $data['opencart_website_full_details'] = $this->Product_migration_model->get_selected_opencart_website_details($data['opencart_url']);
-      foreach ($data['opencart_website_full_details'] as $key => $value) {
-        switch ($key) {
-          case 'opencart_database':
-            $data['opencart_database_name'] = $config['database'] = "$value";
-            break;
-          case 'opencart_dbusername':
-            $config['username'] = "$value";
-            break;
-          case 'opencart_dbpassword':
-            $config['password'] = "$value";
-            break;
-          case 'opencart_dbhost':
-            $config['hostname'] = "$value";
-            break;
-        }
-      }
-      $config['dbdriver'] = 'mysqli';
-      $config['dbprefix'] = '';
-      $config['pconnect'] = TRUE;
-      $config['db_debug'] = TRUE;
-      $config['cache_on'] = FALSE;
-      $config['cachedir'] = '';
-      $config['char_set'] = 'utf8';
-      $config['dbcollat'] = 'utf8_general_ci';
-      $data['opencart_database_connection_status'] = $opencart_database = $this->load->database($config, TRUE);
-
+      $setting_data = $this->Product_migration_model->get_selected_magento_website_details($data['magento_url']);
+      
+     //Set magenot database config dynamically
+      $magento_config_app = switch_db_dynamic($setting_data->magento_dbhost,$setting_data->magento_dbusername,$setting_data->magento_dbpassword,$setting_data->magento_database);
+      $this->magento_db = $this->load->database($magento_config_app,true); 
+      
+    
+      //Set opencart database config dynamically
+      $opencart_config_app = switch_db_dynamic($setting_data->opencart_dbhost,$setting_data->opencart_dbusername,$setting_data->opencart_dbpassword,$setting_data->opencart_database);
+      $this->opencart_db = $this->load->database($opencart_config_app,true); 
+      
       // Create Magento API connection.
       require('Marest.php');
-      $data['magento_api_connection_status'] = $this->api=new Marest($data['magento_url']);
-      $this->api->connect($data['magento_website_admin'], $data['magento_website_admin_password']);
+      $data['magento_api_connection_status'] = $this->api=new Marest($setting_data->magento_url);
+      $this->api->connect($setting_data->magento_admin, $setting_data->magento_admin_password);
 
 
 
       // Starting category migration.
       $this->Product_migration_model->create_product_category_mapping_table($data['opencart_database_name']);
+      $opencartdb = $this->Product_migration_model->opencart_checkquery($data['opencart_website_full_details']->opencart_dbusername,$data['opencart_website_full_details']->opencart_dbpassword,$data['opencart_website_full_details']->opencart_database);
+      echo "oc";print_r($opencartdb);exit;
+     
       $data['opencart_category_details']=$this->Product_migration_model->get_opencart_category_details($data['opencart_database_name']);
       foreach ($data['opencart_category_details'] as $row) {
         foreach ($row as $key => $value) {
