849deacf843e8c26b18ab1f5150b0693ebc178ea
sanket1203 <36723271+sanket1203@users.noreply.github.com>
2019-03-18 14:56:30 +0530

    Model update product

--- application/models/Product_migration_model.php
+++ application/models/Product_migration_model.php
@@ -1,9 +1,3 @@
-/*
-Sanket Patel
-Date: 2019.03.18
-Summary : Migration Controller Class
-*/
-
 <?php
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
@@ -41,33 +35,35 @@ class Product_migration_model extends CI_Model {
 
 
     public function create_product_category_mapping_table($oc_database_name){
-      $query=$this->db->query("CREATE TABLE IF NOT EXISTS product_category_mapping (magento_category_id VARCHAR(250), magento_category_parent VARCHAR(250) ) ");
+      $query=$this->db->query("CREATE TABLE IF NOT EXISTS product_category_mapping (opencart_category_id VARCHAR(250), opencart_category_parent VARCHAR(250), magento_category_id VARCHAR(250), magento_category_parent VARCHAR(250) ) ");
 
      // $query=$this->db->query("ALTER TABLE product_category_mapping ADD magento_category_id VARCHAR(250), ADD magento_category_parent VARCHAR(250)");
     }
     
-    public function opencart_checkquery($opencart_dbusername,$opencart_dbpassword,$opencart_database){
-        if($opencart_dbusername){
-
-$db['mydb2']['username'] = $opencart_dbusername;
-$db['mydb2']['password'] = $opencart_dbpassword;
-$db['mydb2']['database'] = $opencart_database;
-
-
-            $this->config->set_item('mydb2', $db);
-            $this->db2 = $this->load->database('mydb2',true); 
-
-        }
-     
-        $query = $this->db2->query("SELECT category_id AS opencart_category_id, parent_id AS opencart_category_parent
+    public function opencart_checkquery($opencart_db,$opencart_database){
+        
+        $query = $opencart_db->query("SELECT category_id AS opencart_category_id, parent_id AS opencart_category_parent
                             FROM $opencart_database.oclp_category_description JOIN $opencart_database.oclp_category USING (category_id)
                             ORDER BY opencart_category_parent ASC");
-          return $query->result();                    
+        return $query->result();                    
+    }
+    
+    /* save mapping data */
+	public function insert_mapping_data( $data ) {        
+        // Save data
+        $this->db->insert("product_category_mapping", $data);        
+        // Verify if data was saved
+        if ( $this->db->affected_rows() ) {            
+            // Return inserted id
+            return $this->db->insert_id();            
+        } else {            
+            return false;            
+        }        
     }
 
-    public function get_opencart_category_details($oc_database_name){
-      $query=$this->db->query("SELECT category_id, name, parent_id
-                              FROM $oc_database_name.oclp_category_description JOIN $oc_database_name.oc_category USING (category_id)
+    public function get_opencart_category_details($opencart_db,$oc_database_name){
+      $query = $opencart_db->query("SELECT category_id, name, parent_id
+                              FROM $oc_database_name.oclp_category_description JOIN $oc_database_name.oclp_category USING (category_id)
                               ORDER BY parent_id ASC");
       return $query->result_array();
     }
